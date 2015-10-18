29d2aa8ee74f34c54a450bdc56f8b26dcdd4fc0f
Sam Verschueren <sam.verschueren@gmail.com>
2015-10-18 21:01:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# vscode-final-newline
+
+> Inserts a final newline when saving the document.
+
+
+## Install
+
+Press <kbd>F1</kbd> and narrow down the list commands by typing `extension`. Pick `Extensions: Install Extension`.
+
+![](https://github.com/SamVerschueren/vscode-linter-xo/raw/master/screenshot.png)
+
+Simply pick the `final-newline` extension from the list
+
+## Usage
+
+Enable the newline insertion when saving the document in the VSCode Settings.
+
+```json
+{
+  "files.files.insertFinalNewline": true
+}
+```
+
+## License
+
+MIT © [Sam Verschueren](http://github.com/SamVerschueren)
---
+++ extension.ts
@@ -0,0 +1,56 @@
+'use strict';
+
+import {window, workspace, extensions, Disposable, Position, ReadOnlyMemento} from 'vscode';
+import {EOL} from 'os';
+
+export function activate(disposables: Disposable[]) {
+	const controller = new FinalNewLineController();
+
+	disposables.push(controller);
+}
+
+class FinalNewLineController {
+
+	private _disposable: Disposable;
+
+	constructor() {
+		let subscriptions: Disposable[] = [];
+
+		// Subscribe to onSave
+		workspace.onDidSaveTextDocument(this._onEvent, this, subscriptions);
+
+		this._disposable = Disposable.of(...subscriptions);
+	}
+
+	dispose() {
+		this._disposable.dispose();
+	}
+
+	private _onEvent() {
+		extensions.getConfigurationMemento('files').getValue('insertFinalNewline', false)
+			.then(insertFinalNewline => {
+				if (insertFinalNewline === true) {
+					this._insertNewline();
+				}
+			});
+	}
+
+	private _insertNewline() {
+		const editor = window.getActiveTextEditor();
+		if (!editor) {
+			return;
+		}
+
+		const doc = editor.getTextDocument();
+		const docContent = doc.getText();
+
+		const lastLine = doc.getLineCount();
+		const lastLineContent = doc.getTextOnLine(lastLine);
+
+		if (lastLineContent.length !== 0) {
+			editor.edit(editBuilder => {
+				editBuilder.insert(new Position(lastLine, lastLineContent.length+1), EOL);
+			});
+		}
+	}
+}
