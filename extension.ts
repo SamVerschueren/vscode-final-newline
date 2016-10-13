37087e8c89b4bd41de83c21da2e1e58d1714ec6e
Sam Verschueren <sam.verschueren@gmail.com>
2016-10-13 13:19:59 +0200

    prevent cursor jumping at the end of the file - fixes #9

--- extension.ts
+++ extension.ts
@@ -1,12 +1,14 @@
 'use strict';
 import {
+	window,
 	workspace,
 	Disposable,
 	Position,
 	WorkspaceConfiguration,
 	ExtensionContext,
 	TextDocumentWillSaveEvent,
-	TextEdit
+	TextEdit,
+	Selection
 } from 'vscode';
 import {EOL} from 'os';
 
@@ -22,13 +24,15 @@ class FinalNewLineController {
 
 	private _disposable: Disposable;
 	private _config: WorkspaceConfiguration;
+	private _selections: Selection[];
 
 	constructor(config) {
 		this._config = config;
 
-		let subscriptions: Disposable[] = [];
+		const subscriptions: Disposable[] = [];
 
 		workspace.onWillSaveTextDocument(this._onWillDocumentSave, this, subscriptions);
+		workspace.onDidSaveTextDocument(this._onDidDocumentSave, this, subscriptions);
 		workspace.onDidChangeConfiguration(this._onConfigChanged, this, subscriptions);
 
 		this._disposable = Disposable.from(...subscriptions);
@@ -40,6 +44,11 @@ class FinalNewLineController {
 
 	private _onWillDocumentSave(e: TextDocumentWillSaveEvent) {
 		if (this._config.get('insertFinalNewline', false) === true) {
+			if (window.activeTextEditor) {
+				// Keep track of the current pre-save selections in case the cursor is at the last position
+				this._selections = window.activeTextEditor.selections;
+			}
+
 			const doc = e.document;
 			const lastLine = doc.lineAt(doc.lineCount - 1);
 
@@ -50,6 +59,14 @@ class FinalNewLineController {
 					edit
 				]));
 			}
+		}
+	}
+
+	private _onDidDocumentSave() {
+		if (this._selections) {
+			// Put back the selection before the save
+			window.activeTextEditor.selections = this._selections;
+			this._selections = undefined;
 		}
 	}
 
